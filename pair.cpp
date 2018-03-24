85a85a3f51de62be714d4d7390223b56d33dafc1
huwenyue <1151503282@qq.com>
2018-03-24 17:02:45 +0800

    Update and rename main.cpp to pair.cpp

--- pair.cpp
+++ pair.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <utility>
+#include <map>
 using namespace std;
 int main(){
     pair<string, int> p;
@@ -11,5 +11,12 @@ int main(){
     cout<<p.first<<" "<<p.second<<endl;
     p =pair<string, int>("hihi",5);
     cout<<p.first<<" "<<p.second<<endl;
+    
+    mao<string, int> mp;
+    mp.insert(make_pair("heihei",555);
+    mp.insert(pair<string, int<("hh",10);
+    for(map<string int>::iterator it = mp.begin();it!=mp.end();it++){
+         cout<<it->first<<" "<<it->second<<endl;
+    }
     return 0;
 }
